50d7dd31b0e3b64027300d2caa1e8ddfb0e10aed
gutocenci <gutocencigoogle@hotmail.com>
2016-12-06 20:14:08 -0200

    AndroidisIZI

---
+++ MyApplication2/app/src/main/java/gutavocenci/com/br/myapplication/TESTEXPERIA.java
@@ -0,0 +1,13 @@
+package gutavocenci.com.br.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TESTEXPERIA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testexperi);
+    }
+}
---
+++ googlemaps/app/src/main/java/com/example/gustavo/googlemaps/googlemaps.java
@@ -0,0 +1,13 @@
+package com.example.gustavo.googlemaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class googlemaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemaps);
+    }
+}
